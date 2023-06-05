5ca6936edd55bea3c5931e37c9e512e03ef3bc8b
Noobwastaken99 <125329223+Noobwastaken99@users.noreply.github.com>
2023-06-05 18:26:30 -0500

    created user model

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
@@ -1,18 +1,18 @@
 package org.launchcode.javawebdevtechjobsauthentication.models;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
-
-import static sun.nio.fs.UnixPath.encoder;
 
 @Entity
 public class User extends  AbstractEntity {
 
     @NotNull
     private String username;
-
     @NotNull
     private String pwHash;
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public User() {}
 
@@ -23,6 +23,10 @@ public class User extends  AbstractEntity {
 
     public String getUsername() {
         return username;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
     }
 
 }
